96392ffdab7c464e522cf0e0d9812d3c23e50480
renato <rhermoza145@gmail.com>
2022-07-25 12:30:15 +0930

    add setup

--- setup.py
+++ setup.py
@@ -4,20 +4,18 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 requirements = [
-    "fastai<2",
-    "bcolz",
-    "nibabel"
+    "fastai=2.5.2",
 ]
 
 setuptools.setup(
-    name="posthocos",
+    name="casurv",
     version="0.0.1",
     author="Renato Hermoza",
     author_email="renato.hermozaaragones@adelaide.edu.au",
-    description="Post-hoc Overall Survival Time Prediction from Brain MRI",
+    description="Censor-aware Semi-supervised Learning for Survival Time Prediction from Medical Images",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/renato145/rpsalweaklydet",
+    url="https://github.com/renato145/CASurv",
     install_requires=requirements,
     python_requires='>=3.6',
     packages=setuptools.find_packages(),
